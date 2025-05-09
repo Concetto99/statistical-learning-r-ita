726c0cfaede169d756921c12d69538bef74d6b10
concetto99 <marinoconcetto99@gmail.com>
2025-05-09 22:11:44 +0200

    Basic commands - Start repo

---
+++ 01_Ch2_Basic_Commands.r
@@ -0,0 +1,18 @@
+# Rimuovere tutti gli oggetti e pulire il current workspace
+remove(list=ls())
+
+# Farsi restituire la current directory
+getwd()
+
+# Visualizzare tutte le librerie presenti
+installed.packages()
+
+# Installare una libreria
+install.packages("MASS")
+
+# Caricare una libreria
+library("MASS")
+
+# Creare un vettore di 4 elementi
+x <- c(1, 3, 2, 5)
+x
