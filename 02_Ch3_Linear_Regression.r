b5e4a7ecc2567ef77e73bced252bed365d9bddd7
concetto99 <marinoconcetto99@gmail.com>
2025-05-16 00:12:30 +0200

    Add comment on Linear Reg file

--- 02_Ch3_Linear_Regression.r
+++ 02_Ch3_Linear_Regression.r
@@ -54,29 +54,140 @@ summary(lm.fit)
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# Attraverso la funzione names() è possibile visualizzare gli attributi
+# dell'oggetto lm.fit
 names(lm.fit)
 
+# La funzione coef() applicata ad un oggetto di classe lm restituisce in output
+# i coefficienti del modello lineare, ovvero beta0 e beta1 in questo caso
 coef(lm.fit)
 
+# Attraverso la funzione confint() applicata ad un oggetto di classe lm è
+# possibile ottenere gli intervalli di confidenza per i coefficienti beta
+# Default: level = 0.95
+?confint.lm
 confint(lm.fit)
 
+# La funzione predict() applicata ad un oggetto di classe lm ci permette di
+# calcolare i valori predetti ottenuti applicando la funzione di regressione
+# a dei nuovi dati passati mediante un data frame
+
+# Attraverso la funzione predict, con parametri object = lm.fit, newdata
+# che viene passato mediante coercizione di un data frame a 3 valori distinti
+# il che equivale a calcolare i valori per un ipotetico dataset di test e
+# infine esplicitando il tipo di intervallo (confidenza o previsione) associato
+# al valore predetto
+?predict.lm
 predict(lm.fit, data.frame(lstat = (c(5, 10, 15))), interval = "confidence")
-
+# Restituisce i valori predetti della variabile dipendente per lstat = 5, 10, 15
+# includendo l'intervallo di confidenza per il valore medio atteso della
+# risposta
 predict(lm.fit, data.frame(lstat = (c(5, 10, 15))), interval = "prediction")
+# Restituisce i valori predetti per lstat = 5, 10, 15
+# includendo l'intervallo di previsione per una singola nuova osservazione
+# (più ampio rispetto all'intervallo di confidenza)
 
+
+# Visualizzazione grafica della relazione tra la variabile indipendente 'lstat'
+# (percentuale di popolazione a basso reddito) e la variabile dipendente 'medv'
+# (valore mediano delle abitazioni in $1000)
+
+# Attraverso il comando attach() si rendono direttamente accessibili le
+# variabili del dataset Boston
+attach(Boston)
+
+# Attraverso la funzione plot() è possibile ottenere un grafico a dispersione
+# tra le variabili lstat (asse delle ascisse) e medv (asse delle ordinate)
+par(mfrow = c(1, 1)) # Settare la finestra grafica
 plot(lstat, medv)
+
+# Attraverso il comando abline() associato ad un oggetto di classe lm
+# aggiungiamo la retta di regressione stimata del modello al grafico
 abline(lm.fit)
-abline(lm.fit, lwd = 3)
-abline(lm.fit, lwd = 3, col = "red")
+abline(lm.fit, lwd = 3) # linea con spessore pari a 3
+abline(lm.fit, lwd = 3, col = "red") # linea con spessore pari a 3 e colore rosso
 
-
+# Attraverso i comandi sottostanti, in seguito all'impostazione di una
+# finestra grafica di 4 grafici disposti in 2 righe e due colonne,
+# è possibile visualizzare dei grafici con diversi attributi per poter
+# apprezzare le differenze tra essi
+par(mfrow = c(2, 2))
 plot(lstat, medv, col = "red")
 plot(lstat, medv, pch = 20)
 plot(lstat, medv, pch = "+")
 plot(1:20, 1:20, pch = 1:20)
 
-par(mfrow = c(2, 2))
+# La funzione plot associata ad un oggetto di classe lm restituisce in output
+# alcuni grafici che permettono di effettuare la diagnostica dei residui.
+
+# I grafici possibili sono:
+
+    # 1. "Residuals vs Fitted", aka ‘Tukey-Anscombe’ plot
+    # 
+    # 2. "Residual Q-Q" plot
+    # 
+    # 3. "Scale-Location"
+    # 
+    # 4. "Cook's distance"
+    # 
+    # 5. "Residuals vs Leverage"
+    # 
+    # 6. "Cook's dist vs Lev./(1-Lev.)"
+
+# I grafici di default  1, 2, 3 e 5. Possiamo decidere noi usando passando il
+# vettore dei grafici che desideriamo come parametro "which"
+?plot.lm
 plot(lm.fit)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 1. "Residuals vs Fitted", aka ‘Tukey-Anscombe’ plot
+
+# Il grafico in alto a sinistra mostra i residui (ordinate) in funzione dei
+# valori predetti (ascisse) del modello. Questo grafico è molto utile in quanto
+# aiuta a verificare se rimane struttura residua non spiegata dalle variabili
+# del modello e quindi è necessario inglobarne delle altre
+# Idealmente i punti dovrebbero essere distribuiti casualmente intorno alla
+# linea orizzontale a zero. La curva all'interno del grafico viene ottenuta
+# mediante il metodo LOESS (LOcal regrESSion)
+
+# 2. "Residual Q-Q" plot
+
+# Il grafico in alto a destra mostra nell'asse delle ascisse i quantili teorici
+# di una distribuzione normale e nell'asse delle ordinate i residui
+# studentizzati. Questo grafico è molto utile in quanto ci da un'indicazione su
+# come i residui si distribuiscano. Se la curva formata fosse esattamente
+# una linea retta passante per l'origine ci troveremmo di fronte ad una
+# distribuzione dei residui esattamente normale. Viceversa si tratterebbe di
+# una distribuzione asimmetrica. Questo controllo è abbastanza importante
+# in quanto è richiesta l'ipotesi fondamentale di Normalità per la
+# correttezza dei test statistici sui coefficienti, intervalli di confidenza
+# e affidabilità delle previsioni
+
+# 3. "Scale-Location"
+
+# Il grafico in basso a sinistra mostra la radice quadrata dei residui
+# standardizzati (asse delle ordinate) rispetto ai fitted values (asse delle
+# ascisse). Questo grafico è utile per rilevare eventuali problemi di
+# eteroschedasticità, ovvero una varianza non costante dei residui al
+# variare dei valori predetti.
+
+# 5. "Residuals vs Leverage"
+
+# Il grafico in basso a destra mostra l'indice di leva (asse delle ascisse),
+# ovvero la misura dell’influenza potenziale di un punto, e i residui
+# studentizzati (asse delle ordinate). La misura di Leverage indica, quindi,
+# quanto un'osservazione è estrema e quindi influenza i valori delle stime
+# dei coefficienti.
+# Le linee rosse tratteggiate rappresentano i contorni delle distanze di Cook,
+# che combinano leverage e grandezza del residuo per identificare i punti
+# potenzialmente influenti.
+# In regressione lineare semplice, il valore medio del leverage è
+# approssimativamente pari a (p+1)/n
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 plot(predict(lm.fit), residuals(lm.fit))
 plot(predict(lm.fit), rstudent(lm.fit))
@@ -116,7 +227,8 @@ summary(lm(medv ~ log(rm), data = Boston))
 
 head(Carseats)
 
-summary(lm.fit)
+
 lm.fit <- lm(Sales ~ . + Income:Advertising + Price:Age, data = Carseats)
+summary(lm.fit)
 
 contrasts(Carseats$ShelveLoc)
